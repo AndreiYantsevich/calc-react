71b3ebbdaf8fdb4d8bc17213850e2290f136b428
Andrei <yantsevich92@gmail.com>
2022-01-19 22:40:19 +0300

    completed wrote the calculator logic

--- src/App.js
+++ src/App.js
@@ -3,51 +3,64 @@ import './App.css'
 import {Display} from "./components/Display/Display";
 
 function App() {
-    const [result, setResult] = useState(0);
+    const [result, setResult] = useState('');
 
-    const handleButtonPress = (e) => {
-        setResult(result + e.currentTarget.name)
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name))
     }
 
     const clear = () => {
-        setResult(0)
+        setResult('')
     }
 
-    const handlePositiveToNegative = () => {
-        setResult(-result)
+    const backspase = () => {
+        setResult(result.slice(0, -1))
     }
 
+    const percenter = () => {
+        setResult((result / 100).toString())
+    }
+
+    const calculate = () => {
+        try {
+            setResult(eval(result).toString())
+        } catch (error) {
+            setResult('Error')
+        }
+    }
 
     return (
         <div className='calc'>
             <Display value={result}/>
             <div className='buttons'>
-                <button name='AC' className='btn bg-grey' onClick={clear}>{result !== 0 ? 'C' : 'AC'}
+                <button name='AC' className='btn bg-grey'
+                        onClick={clear}>AC
                 </button>
-                <button name='+/-' className='btn bg-grey' onClick={handlePositiveToNegative}>+/-
+                <button name='C' className='btn bg-grey'
+                        onClick={backspase}>C
                 </button>
-                <button name='%' className='btn bg-grey' onClick={handleButtonPress}>%
+                <button name='%' className='btn bg-grey' onClick={percenter}>%
                 </button>
-                <button name='/' className='btn bg-orange' onClick={handleButtonPress}>/
+                <button name='/' className='btn bg-orange' onClick={handleClick}>/
                 </button>
-                <button name='7' className='btn' onClick={handleButtonPress}>7</button>
-                <button name='8' className='btn' onClick={handleButtonPress}>8</button>
-                <button name='9' className='btn' onClick={handleButtonPress}>9</button>
-                <button name='x' className='btn bg-orange' onClick={handleButtonPress}>x
+                <button name='7' className='btn' onClick={handleClick}>7</button>
+                <button name='8' className='btn' onClick={handleClick}>8</button>
+                <button name='9' className='btn' onClick={handleClick}>9</button>
+                <button name='*' className='btn bg-orange' onClick={handleClick}>x
                 </button>
-                <button name='4' className='btn' onClick={handleButtonPress}>4</button>
-                <button name='5' className='btn' onClick={handleButtonPress}>5</button>
-                <button name='6' className='btn' onClick={handleButtonPress}>6</button>
-                <button name='-' className='btn bg-orange' onClick={handleButtonPress}>-
+                <button name='4' className='btn' onClick={handleClick}>4</button>
+                <button name='5' className='btn' onClick={handleClick}>5</button>
+                <button name='6' className='btn' onClick={handleClick}>6</button>
+                <button name='-' className='btn bg-orange' onClick={handleClick}>-
                 </button>
-                <button name='1' className='btn' onClick={handleButtonPress}>1</button>
-                <button name='2' className='btn' onClick={handleButtonPress}>2</button>
-                <button name='3' className='btn' onClick={handleButtonPress}>3</button>
-                <button name='+' className='btn bg-orange' onClick={handleButtonPress}>+
+                <button name='1' className='btn' onClick={handleClick}>1</button>
+                <button name='2' className='btn' onClick={handleClick}>2</button>
+                <button name='3' className='btn' onClick={handleClick}>3</button>
+                <button name='+' className='btn bg-orange' onClick={handleClick}>+
                 </button>
-                <button name='0' className='btn zero' onClick={handleButtonPress}>0</button>
-                <button name=',' className='btn' onClick={handleButtonPress}>,</button>
-                <button name='=' className='btn bg-orange' onClick={handleButtonPress}>=
+                <button name='0' className='btn zero' onClick={handleClick}>0</button>
+                <button name='.' className='btn' onClick={handleClick}>.</button>
+                <button name='=' className='btn bg-orange' onClick={calculate}>=
                 </button>
             </div>
         </div>
